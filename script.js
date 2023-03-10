f81d665b9a4ab69a30e838c4ff1d55ebb95167da
johnnysza <johnsilvasza@gmail.com>
2023-03-10 02:57:05 -0300

    feat: código finalizado

--- script.js
+++ script.js
@@ -1,41 +1,32 @@
-const draggable = document.querySelectorAll(".item");
-const droppable = document.querySelector(".dropbox");
+// Determinando a tag e class que serão selecionadas
+const wrapper = document.querySelector(".wrapper"),
+  header = wrapper.querySelector("header");
 
-draggable.forEach((item) => {
-  item.addEventListener("dragstart", () => {
-    item.classList.add("dragging");
-  });
-  item.addEventListener("dragend", () => {
-    item.classList.remove("dragging");
-  });
+function onDrag({ movementX, movementY }) {
+  // Pegando os valores do HTML e propriedades CSS da class selecionada
+  let getStyle = window.getComputedStyle(wrapper);
+
+  // Pega os valores das posiçoes left e top da div, transformando-os em inteiros.
+  let leftVal = parseInt(getStyle.left);
+  let topVal = parseInt(getStyle.top);
+
+  // Lê os inteiros equivalentes a posição da div.
+  wrapper.style.left = `${leftVal + movementX}px`;
+  wrapper.style.top = `${topVal + movementY}px`;
+}
+
+// Evento é acionado clicando com o botão esquerdo.
+header.addEventListener("mousedown", () => {
+  header.classList.add("active");
+
+  // Div movimentada no clique.
+  header.addEventListener("mousemove", onDrag);
 });
 
-droppable.forEach((zone) => {
-  zone.addEventListener("dragover", (e) => {
-    e.preventDefault();
+// Evento é acionado soltando o botão esquerdo.
+document.addEventListener("mouseup", () => {
+  header.classList.remove("active");
 
-    const curItem = document.querySelector(".is-dragging");
-
-    zone.appendChild(curItem);
-  });
+  // Interrompe a movimentação da div.
+  header.removeEventListener("mousemove", onDrag);
 });
-
-const insertAboveItem = (zone, mouseY) => {
-  const els = zone.querySelectorAll(".item:not(.is-dragging)");
-
-  let closestItem = null;
-  let closestOffset = Number.NEGATIVE_INFINITY;
-
-  els.forEach((item) => {
-    const { top } = item.getBoundingClientRect();
-
-    const offset = mouseY - top;
-
-    if (offset < 0 && offset > closestOffset) {
-      closestOffset = offset;
-      closestItem = task;
-    }
-  });
-
-  return closestItem;
-};
