1552618aa387ae02bf11926937f32af4d67411f5
johnnysza <johnsilvasza@gmail.com>
2023-03-09 22:04:06 -0300

    adicionado novas funcionalidades

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const draggable = document.querySelectorAll(".item");
-const droppable = document.querySelectorAll(".dropbox");
+const droppable = document.querySelector(".dropbox");
 
 draggable.forEach((item) => {
   item.addEventListener("dragstart", () => {
@@ -14,8 +14,28 @@ droppable.forEach((zone) => {
   zone.addEventListener("dragover", (e) => {
     e.preventDefault();
 
-    const curTask = document.querySelector(".is-dragging");
+    const curItem = document.querySelector(".is-dragging");
 
-    zone.appendChild(curTask);
+    zone.appendChild(curItem);
   });
 });
+
+const insertAboveItem = (zone, mouseY) => {
+  const els = zone.querySelectorAll(".item:not(.is-dragging)");
+
+  let closestItem = null;
+  let closestOffset = Number.NEGATIVE_INFINITY;
+
+  els.forEach((item) => {
+    const { top } = item.getBoundingClientRect();
+
+    const offset = mouseY - top;
+
+    if (offset < 0 && offset > closestOffset) {
+      closestOffset = offset;
+      closestItem = task;
+    }
+  });
+
+  return closestItem;
+};
